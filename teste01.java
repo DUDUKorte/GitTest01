c041d4e01eca7f4bc162e809ec6745a31969b5de
DUDUKorte <dudu.korte@gmail.com>
2023-03-03 15:58:17 -0300

    Outro Hello World mas com modificação

--- teste01.java
+++ teste01.java
@@ -1,5 +1,5 @@
 public class teste01{
     public static void main(String args[]){
-        System.out.println("Hello World!");
+        System.out.println("Hello World! mas com uma modificação diferenciada");
     }
 }
